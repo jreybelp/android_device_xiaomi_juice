bb24fea4f581ef7f5df8cee1219d81f1bea15956
GtrCraft <nickvanbokhorst@hotmail.com>
2021-05-03 11:34:03 +0200

    juice: Disable HW overlays
    Change-Id: Iea79079a97ef9e919cc2f6aa22827c7394d92af3

--- product_prop.mk
+++ product_prop.mk
@@ -3,3 +3,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
+    
+# Always use GPU for screen compositing
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_hwc_overlays=1
