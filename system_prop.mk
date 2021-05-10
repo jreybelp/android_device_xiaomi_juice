a634ad60d51119991a8a8dcaee0fa66e985b9567
ѕαмαя νιѕρυтє <samarvispute121@gmail.com>
2021-05-10 01:37:57 +0000

    juice: add memory optimization props

--- system_prop.mk
+++ system_prop.mk
@@ -7,3 +7,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.force_ltd_sys_ind=1 \
     persist.vendor.radio.voice_on_lte=1 \
     persist.vendor.radio.calls.on.ims=1
+
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000 \
+    ro.am.reschedule_service=true \
+    ro.sys.fw.bg_apps_limit=24
