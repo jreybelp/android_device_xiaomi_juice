b97bd3e48053e291faa31d5feed46afdc19a0d84
Joaquin Guzman <githubjoaquin@gmail.com>
2021-06-18 17:14:34 +0000

    juice: nuke triple frame buffers

--- product_prop.mk
+++ product_prop.mk
@@ -2,7 +2,6 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     debug.sf.hw=1 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
     
 # Always use GPU for screen compositing
