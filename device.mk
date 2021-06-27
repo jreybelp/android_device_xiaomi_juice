99a6a49b3e4c1b72d8399e4f4585b195fa1cf9af
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-06-27 12:45:23 +0530

    juice: Forget to address AIDL Power Hal
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- device.mk
+++ device.mk
@@ -107,6 +107,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service.juice \
     vendor.qti.hardware.perf@2.0 \
     vendor.qti.hardware.perf@2.1 \
     vendor.qti.hardware.perf@2.2
