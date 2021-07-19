2c8ffe4b81f5d7f90f373eeb8894346488bee8f5
Joaquin Guzman <githubjoaquin@gmail.com>
2021-07-19 04:01:00 +0000

    juice: update redfin to july release

--- havoc_juice.mk
+++ havoc_juice.mk
@@ -25,4 +25,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
