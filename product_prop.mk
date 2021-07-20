dveofficial/device_xiaomi_juice

juice: Allow use context priority task
Signed-off-by: aseelps <aseelpsathar@gmail.com>

product_prop.mk
@@ -8,6 +8,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.hw=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.running_without_sync_framework=true \
+    ro.surface_flinger.use_context_priority=true \
     debug.sf.latch_unsignaled=0
 
 # Media

jreybelp/android_device_xiaomi_juice

product_prop.mk
@@ -8,7 +8,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     ro.config.avoid_gfx_accel=true \
-    ro.surface_flinger.running_without_sync_framework=true
+    ro.surface_flinger.running_without_sync_framework=true \
+    ro.surface_flinger.use_context_priority=true 
 
 # Always use GPU for screen compositing
 PRODUCT_PROPERTY_OVERRIDES += \