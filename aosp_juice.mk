fded867d1b9e8df062d49c648965fa1453bb56a3
Joaquin Guzman <githubjoaquin@gmail.com>
2021-07-20 02:04:49 +0000

    juice: Hycon-ify

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,18 +6,14 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common havoc stuff.
-WITH_GAPPS := true
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
-
-# Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
