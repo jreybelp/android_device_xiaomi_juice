a95a97c3b028a7cd1bfa326f5435e6af08d37e73
Joaquin Guzman <githubjoaquin@gmail.com>
2021-07-21 16:59:38 +0000

    juice: Descendant Bringup

--- descendant_juice.mk
+++ descendant_juice.mk
@@ -6,9 +6,8 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common havoc stuff.
-WITH_GAPPS := true
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common descendant stuff.
+$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
@@ -17,7 +16,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := descendant_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
