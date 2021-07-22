82e40a8202128954bb1b104c09ccca1a565f6949
Mehmet vural <vuralm88@gmail.com>
2021-07-22 09:11:19 +0000

    juice: pixel Basterbringup

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,9 +6,9 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common havoc stuff.
+# Inherit some common aosp stuff.
 WITH_GAPPS := true
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
@@ -17,7 +17,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
