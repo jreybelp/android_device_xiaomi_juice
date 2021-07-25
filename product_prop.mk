aea6092eb3a5b5e0ed8e49ce4252eab12f1eda83
Joaquin Guzman <githubjoaquin@gmail.com>
2021-07-25 16:16:42 +0000

    juice: enable blur by default

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,8 @@
+# blurs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
