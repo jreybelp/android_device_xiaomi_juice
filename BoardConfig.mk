feb7c10aacd176ea00666f3669232de4b2df3bfb
Martinusbe <martinusbe@gmail.com>
2021-08-02 07:58:54 +0000

    juice: Adapt the exact values for lockscreen charging info Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com> Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,10 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 # Display
 TARGET_USES_HWC2 := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+HEALTHD_USE_BATTERY_INFO := true
+
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
 
