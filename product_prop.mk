88753893eb60a61a7198cf4626dd23e7c36d388c
John Paul Patigas <johnpaulpatigas@gmail.com>
2021-08-02 07:51:46 +0000

    juice: Switch to opengl as UI renderer

--- product_prop.mk
+++ product_prop.mk
@@ -9,7 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.hwui.renderer=skiagl \
+    debug.hwui.renderer=opengl \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.running_without_sync_framework=true \
