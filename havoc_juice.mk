60e5aa399e3337eeb2cd8fcb33d2c4bb49b03cba
Joaquin Guzman <githubjoaquin@gmail.com>
2021-08-06 02:32:09 +0000

    juice: update Redfin FP to August Patch

--- havoc_juice.mk
+++ havoc_juice.mk
@@ -24,4 +24,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
