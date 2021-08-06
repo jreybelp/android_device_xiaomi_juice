3961acf266103862825e3f5a736afd7822d5b73e
Joaquin Guzman <githubjoaquin@gmail.com>
2021-08-06 06:03:50 +0000

    juice: prop:nuke scrolling optimizations

--- system_prop.mk
+++ system_prop.mk
@@ -38,10 +38,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q6115-31409-1
 
-# Scroll optimization
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.perf.scroll_opt=true
-
 # ZRAM
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
