80acf1a20f0e65ff5e6ffee82ebb853ff3d0a77f
Joaquin Guzman <githubjoaquin@gmail.com>
2021-10-07 18:10:08 +0800

    juice: Buid QTI Power hal

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.juice \
+    android.hardware.power-service.qti \
     vendor.qti.hardware.perf@2.0 \
     vendor.qti.hardware.perf@2.1 \
     vendor.qti.hardware.perf@2.2
