eb8192ad7af04920fb77d3acd7da46a9c9fbf210
Joaquin Guzman <githubjoaquin@gmail.com>
2021-10-10 00:24:06 +0800

    juice: Arcane-ify!!!

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,17 +6,20 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Arcane OS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_PIXEL_CHARGER  := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
 TARGET_GAPPS_ARCH := arm64
+BUILD_TYPE_IS_OFFICIAL := true
 
 # Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -25,6 +28,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
-
-# Havoc Official
-HAVOC_BUILD_TYPE := Official
