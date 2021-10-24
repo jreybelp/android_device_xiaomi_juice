4beff54fc57f7e2f987ea259421beb2306b283c7
TH779 <i@779.moe>
2021-10-24 12:41:50 +0800

    juice: Disable settings provider model  * When it is enabled, if we have multiple SIM cards, the networks    settings UX will only show the first SIM card.  * Thanks to Telegram@CyberPhantom52 for the fix patch.
    Signed-off-by: TH779 <i@779.moe>
    Signed-off-by: Joaquin Guzman <githubjoaquin@gmail.com>
    Change-Id: I2690e6dcd69f8e0fe814ea3c92c85d7c6f894166

--- system_prop.mk
+++ system_prop.mk
@@ -40,6 +40,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q6115-31409-1
 
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.fflag.override.settings_provider_model=false
+
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.binary_xml=false
