ab6a6f68a1850633240a92fa06f9ecc51e217f13
Joaquin Guzman <githubjoaquin@gmail.com>
2021-11-01 05:22:07 +0000

    juice: prop: Enable debug.egl.hw & debug.egl.force.msaa Signed-off-by: Joaquin Guzman <githubjoaquin@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -20,6 +20,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.disable_hwc_overlays=1 \
     debug.sdm.support_writeback=0 \
     debug.enable.sglscale=1 \
+    debug.egl.hw=1 \
+    debug.egl.force.msaa=1 \
     debug.mdpcomp.logs=0 \
     debug.sf.recomputecrop=0 \
     vendor.display.disable_rotator_downscale=1 \
@@ -38,10 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=15 \
     vendor.audio.offload.buffer.size.kb=256
 
-# Always use GPU for screen compositing
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_hwc_overlays=1
-
 # Power-saving props
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.use_data_netmgrd=true \
@@ -54,3 +52,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true \
     persist.device_config.configuration.disable_rescue_party=true
+
