8b899a249d586f0de270cc63fee8e2a87fb5ce38
Joaquin Guzman <githubjoaquin@gmail.com>
2021-11-22 14:20:51 +0800

    juice: Added Prebuilt GCam-go

--- device.mk
+++ device.mk
@@ -58,9 +58,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor
 
-# Camera
+# GCam-Go
 PRODUCT_PACKAGES += \
-    Snap
+    GCam
 
 # Display
 PRODUCT_PACKAGES += \
