d3328f5bbb9e909cf90260da061657ea536d0a2c
Don Joaquin <githubjoaquin@gmail.com>
2021-12-21 12:53:25 +0000

    Revert "juice: prop: Enable debug.egl.hw & debug.egl.force.msaa"
    This reverts commit ab6a6f68a1850633240a92fa06f9ecc51e217f13.

--- product_prop.mk
+++ product_prop.mk
@@ -23,8 +23,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.disable_hwc_overlays=1 \
     debug.sdm.support_writeback=0 \
     debug.enable.sglscale=1 \
-    debug.egl.hw=1 \
-    debug.egl.force.msaa=1 \
     debug.mdpcomp.logs=0 \
     debug.sf.recomputecrop=0 \
     vendor.display.disable_rotator_downscale=1 \
@@ -43,6 +41,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=15 \
     vendor.audio.offload.buffer.size.kb=256
 
+# Always use GPU for screen compositing
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_hwc_overlays=1
+
 # Power-saving props
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.use_data_netmgrd=true \
@@ -55,4 +57,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true \
     persist.device_config.configuration.disable_rescue_party=true
-
