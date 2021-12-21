8e5e8c5bf8ffafd301a9955390ccdd5eb9f1c608
Don Joaquin <githubjoaquin@gmail.com>
2021-12-21 12:54:51 +0000

    juice: Fix Derp!

--- product_prop.mk
+++ product_prop.mk
@@ -41,10 +41,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=15 \
     vendor.audio.offload.buffer.size.kb=256
 
-# Always use GPU for screen compositing
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_hwc_overlays=1
-
 # Power-saving props
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.use_data_netmgrd=true \
