d2e0b21ecf02381e3b06f5fd2274d2a438e6983a
Don Joaquin <githubjoaquin@gmail.com>
2021-12-21 12:47:21 +0000

    juice: Update Raven FP to December Patch

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -24,4 +24,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
