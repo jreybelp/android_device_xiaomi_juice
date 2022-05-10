fad90824b113c72cc26b3f7372bfda6beaf9564d
Cenzooo <falihafiq1928@gmail.com>
2022-05-10 23:50:17 +0700

    Update and rename aosp_juice.mk to rohie_juice.mk

--- rohie_juice.mk
+++ rohie_juice.mk
@@ -7,16 +7,15 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/rohie/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
 # Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := rohie_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
