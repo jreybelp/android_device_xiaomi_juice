81b4aa809b4b8197ed29944c99dd76dacfdc4703
Cenzooo <falihafiq1928@gmail.com>
2022-05-18 01:46:52 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/corvus_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    corvus_juice-user \
+    corvus_juice-userdebug \
+    corvus_juice-eng
